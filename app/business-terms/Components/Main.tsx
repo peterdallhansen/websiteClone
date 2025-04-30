d9ccdc727cdd524b9727c848dc2ff4eff9df0224
Peter Dall-Hansen <peterdallhanasen@gmail.com>
2025-04-30 11:20:13 +0200

    refactor: Remove unused BlurFade import from Main.tsx

--- app/business-terms/Components/Main.tsx
+++ app/business-terms/Components/Main.tsx
@@ -1,5 +1,4 @@
 "use client";
-import BlurFade from "@/components/ui/blur-fade";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import dayjs from "dayjs";
